eae6cc0d572b16864e74b5675c325c27a694376a
EC2 Default User <ec2-user@ip-172-31-27-238.us-east-2.compute.internal>
2024-09-09 10:07:04 +0000

    reproduce Cx issue

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,54 +1,146 @@
 import { Image, StyleSheet, Platform } from 'react-native';
-
+import { useEffect, useState } from "react";
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
+import { Button, Linking, Text, View } from 'react-native';
+import { Amplify, Auth, Hub } from "aws-amplify";
 
-export default function HomeScreen() {
+Amplify.Logger.LOG_LEVEL = "DEBUG"
+
+async function urlOpener(url: string, redirectUrl: string): Promise<void> {
+  await InAppBrowser.isAvailable();
+  const authSessionResult = await InAppBrowser.openAuth(url, redirectUrl, {
+    showTitle: false,
+    enableUrlBarHiding: true,
+    enableDefaultShare: false,
+    ephemeralWebSession: false,
+  });
+
+  if (authSessionResult.type === 'success') {
+    Linking.openURL(authSessionResult.url);
+  }
+}
+
+
+Amplify.configure({
+  Auth: {
+
+    // REQUIRED - Amazon Cognito Region
+    region: 'us-east-1',
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'us-east-1_zWOf1kIft',
+
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: 'ugutdlestbafg0m7qclit8ujm',
+
+
+    // OPTIONAL - Hosted UI configuration
+    oauth: {
+      domain: 'ptzu-amplify.auth.us-east-1.amazoncognito.com',
+      scope: [
+        'email',
+        'openid'
+      ],
+      redirectSignIn: 'https://www.amazon.com/',
+      redirectSignOut: 'https://management.ntu.edu.tw/IM',
+      responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+    }
+  }
+});
+
+// You can get the current config object
+const currentConfig = Auth.configure();
+
+  
+  
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then(userData => setUser(userData));
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
+
+    getUser().then(userData => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then(userData => userData)
+      .catch(() => console.log('Not signed in'));
+  }
+  
+  async function getGoogleClick(){
+    console.log("google clicking")
+    await Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google });
+    const user = await Auth.currentAuthenticatedUser();
+    console.log("USER = ", user)
+  }
+  
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View>
+      <Text>User: {user ? JSON.stringify(user.attributes) : 'None'}</Text>
+      {user ? (
+        <Button title="Sign Out" onPress={() => Auth.signOut()} />
+      ) : (
+        <>
+          {/* Go to the Cognito Hosted UI */}
+          <Button title="Cognito" onPress={() => Auth.federatedSignIn()} />
+
+          {/* Go directly to a configured identity provider */}
+          <Button title="Facebook" onPress={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Facebook })} />
+          <Button title="222" onPress={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}  />
+          <Button title="1111" onPress={() => getGoogleClick()} />
+          <Button title="Amazon" onPress={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Amazon })} />
+        </>
+      )}
+    </View>
   );
 }
+
+export default App;
+  
+  
+
+// export default function HomeScreen() {
+ 
+//   return (
+// <View>
+//       <Text>User: {user ? JSON.stringify(user.attributes) : 'None'}</Text>
+//       {user ? (
+//         <Button title="Sign Out" onPress={() => Auth.signOut()} />
+//       ) : (
+//         <>
+//           {/* Go to the Cognito Hosted UI */}
+//           <Button title="Cognito" onPress={() => Auth.federatedSignIn()} />
+
+//           {/* Go directly to a configured identity provider */}
+        
+//           <Button title="Google" onPress={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}  />
+        
+//         </>
+//       )}
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   titleContainer: {
