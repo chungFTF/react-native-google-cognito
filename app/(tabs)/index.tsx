3681fd7775ba1bf35b21c925b45baf98cc164805
EC2 Default User <ec2-user@ip-172-31-27-238.us-east-2.compute.internal>
2024-09-10 04:14:55 +0000

    get credential successfully

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,11 @@
-import {
-  Linking,
-  Button,
-  Text,
-  View,
-} from 'react-native';
+import { Linking, Button, Text, View } from "react-native";
 import { useEffect, useState } from "react";
-import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
-import InAppBrowser from 'react-native-inappbrowser-reborn';
+import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth";
+import InAppBrowser from "react-native-inappbrowser-reborn";
 import { Amplify, Auth, Hub } from "aws-amplify";
-import * as WebBrowser from 'expo-web-browser';
+import * as WebBrowser from "expo-web-browser";
 
-Amplify.Logger.LOG_LEVEL = "DEBUG"
+Amplify.Logger.LOG_LEVEL = "DEBUG";
 
 async function urlOpener(url: string, redirectUrl: string): Promise<void> {
   await InAppBrowser.isAvailable();
@@ -21,35 +16,30 @@ async function urlOpener(url: string, redirectUrl: string): Promise<void> {
     ephemeralWebSession: false,
   });
 
-  if (authSessionResult.type === 'success') {
+  if (authSessionResult.type === "success") {
     Linking.openURL(authSessionResult.url);
   }
 }
 
-
 Amplify.configure({
   Auth: {
     // REQUIRED - Amazon Cognito Region
-    region: 'us-east-2',
+    region: "us-east-2",
 
     // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolId: 'us-east-2_NAMeNcssO',
+    userPoolId: "us-east-2_u0DyCMWvW",
 
     // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: '5cc725gen2j2ilr10a6llhg6nf',
+    userPoolWebClientId: "2mpq6knao5rj1dsssv1mtm46ra",
 
     // OPTIONAL - Hosted UI configuration
     oauth: {
-      domain: 'ptzu-google-test.auth.us-east-2.amazoncognito.com',
-      scope: [
-        'profile',
-        'email',
-        'openid'
-      ],
-      redirectSignIn: 'https://www.amazon.com',
-      redirectSignOut: 'https://management.ntu.edu.tw/IM',
-      responseType: 'token', // or 'token', note that REFRESH token will only be generated when the responseType is code
-      signUpVerificationMethod: 'code',
+      domain: "ptzu-google.auth.us-east-2.amazoncognito.com",
+      scope: ["profile", "email", "openid"],
+      redirectSignIn: "steed://",
+      redirectSignOut: "steed://",
+      responseType: "token", // or 'token', note that REFRESH token will only be generated when the responseType is code
+      signUpVerificationMethod: "code",
       urlOpener: async (url, redirectUrl) => {
         try {
           const res = await WebBrowser.openAuthSessionAsync(url, redirectUrl, {
@@ -57,14 +47,13 @@ Amplify.configure({
             enableUrlBarHiding: true,
             enableDefaultShare: false,
             ephemeralWebSession: false,
-            preferEphemeralSession: true
+            preferEphemeralSession: true,
           });
-          console.log('res: ', res);
-        } catch(error) {
-          console.log("ERROR in browser = ", errpr)
+          console.log("res: ", res);
+        } catch (error) {
+          console.log("ERROR in browser = ", error);
         }
-  
-        
+
         // try {
         //   const res = await WebBrowser.openAuthSessionAsync(url, redirectUrl, {
         //     showTitle: false,
@@ -78,62 +67,57 @@ Amplify.configure({
         //     console.log("redirectUrl in web browser = ", redirectUrl)
         //   });
 
-              
         //   console.log("RESPNSE ===", res)
         // } catch(error){
         //   console.log("ERRORRRRR ====", error)
-        // }
+        // }s
+      },
     },
-  }
-  }
+  },
 });
 
 // You can get the current config object
 const currentConfig = Auth.configure();
-  
+
 function App() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    Hub.listen('auth', ({ payload: { event, data } }) => {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
       switch (event) {
-        case 'signIn':
-        case 'cognitoHostedUI':
-          getUser().then(userData => setUser(userData));
+        case "signIn":
+        case "cognitoHostedUI":
+          getUser().then((userData) => setUser(userData));
           break;
-        case 'signOut':
+        case "signOut":
           setUser(null);
           break;
-        case 'signIn_failure':
-        case 'cognitoHostedUI_failure':
-          console.log('Sign in failure', data);
+        case "signIn_failure":
+        case "cognitoHostedUI_failure":
+          console.log("Sign in failure", data);
           break;
       }
     });
 
-    getUser().then(userData => setUser(userData));
+    getUser().then((userData) => setUser(userData));
   }, []);
 
   function getUser() {
     return Auth.currentAuthenticatedUser()
-      .then(userData => userData)
-      .catch(() => console.log('Not signed in'));
+      .then((userData) => userData)
+      .catch(() => console.log("Not signed in"));
   }
-  
-  async function getGoogleClick(){
-    console.log("google clicking")
-    
+
+  async function getGoogleClick() {
+    console.log("google clicking");
+
     let creds = await Auth.federatedSignIn({
       provider: CognitoHostedUIIdentityProvider.Google,
     });
-    
-    console.log("google clicking #1 - creds: ", creds)
-    
+
+    console.log("google clicking #1 - creds: ", creds);
     const user = await Auth.currentAuthenticatedUser();
-    
-    console.log("google clicking #2")
-    
-    console.log("USER = ", user)
+    console.log("google clicking #2 - User: ", user);
   }
 
   return (
@@ -141,14 +125,22 @@ function App() {
       <Text>DUMMY</Text>
       <Text>DUMMY</Text>
       <Text>DUMMY</Text>
-      <Text>User: {user ? JSON.stringify(user.attributes) : 'None'}</Text>
+      <Text>User: {user ? JSON.stringify(user.attributes) : "None"}</Text>
       {user ? (
         <Button title="Sign Out" onPress={() => Auth.signOut()} />
       ) : (
         <>
           <Button title="Cognito" onPress={() => Auth.federatedSignIn()} />
-          <Button title="Google" onPress={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}  />
+          <Button
+            title="Google"
+            onPress={() =>
+              Auth.federatedSignIn({
+                provider: CognitoHostedUIIdentityProvider.Google,
+              })
+            }
+          />
           <Button title="Custom Google" onPress={() => getGoogleClick()} />
+          <Button title="Get USer" onPress={() => getUser()} />
         </>
       )}
     </View>
@@ -156,7 +148,3 @@ function App() {
 }
 
 export default App;
-
-
-
-https://mydomain.us-east-1.amazoncognito.com/login?response_type=code&client_id=1example23456789&redirect_uri=https://www.example.com
